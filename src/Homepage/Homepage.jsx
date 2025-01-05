a3b6d74515dabbad9a439e5cf0f90ed604307454
Aryan <buggy.the.crown.1945@gmail.com>
2025-01-06 04:46:42 +0545

    re-deiting homepage

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -5,7 +5,19 @@ export function Homepage() {
     return(
         <>
             <Navbar />
-            <section> Home </section>
+            <section className="dashboard">
+                <div className="left-section">
+                    <li><a>Dashboard</a></li>
+                    <li><a>Dashboard</a></li>
+                    <li><a>Dashboard</a></li>
+                </div>
+                <div className="profile-section">
+                    Profile
+                </div>
+                <div className="medicals">
+                    Medicals 
+                </div>
+            </section>
         </>
     )
 }
